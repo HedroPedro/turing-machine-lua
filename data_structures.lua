d7bb0589d5288d8748ad34b14f670cb221d320df
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2024-09-22 12:00:36 -0300

    feat: maquina de turing feita

--- data_structures.lua
+++ data_structures.lua
@@ -24,17 +24,31 @@ function DoubleLinkedList.new()
 end
 
 function DoubleLinkedList.append_left(list, value)
-    local node = {before = nil, next = list, value = value}
-    return node
+    local node = {before = nil, next = nil, info = value}
+    if not list then
+        return node
+    end
+
+    local to_append = list
+    while to_append.before do
+        to_append = to_append.before
+    end
+    to_append.before = node
+    node.next = to_append
+    return list
 end
 
 function DoubleLinkedList.append_right(list, value)
-    local node = {before = nil, next = nil, value = value}
-    local tmp_list = list
-    while tmp_list.next do
-        tmp_list = tmp_list.next
+    local node = {before = nil, next = nil, info = value}
+    if not list then
+        return node
     end
-    tmp_list.next = node
+    local to_append = list
+    while to_append.next do
+        to_append = to_append.next
+    end
+    to_append.next = node
+    node.before = to_append
     return list
 end
 
@@ -48,9 +62,28 @@ end
 function DoubleLinkedList.print(list)
     local tmp_list = list
     while tmp_list do
-        print(tmp_list.value)
+        print(tmp_list.info)
         tmp_list = tmp_list.next
     end
+end
+
+function DoubleLinkedList.convert_values_to_string(list)
+    local str = ""
+    local next = list
+
+    if not list then
+        return str
+    end
+
+    while next.before do
+        next = next.before
+    end
+
+    while next do
+        str = str  .. "" .. next.info
+        next = next.next
+    end
+    return str
 end
 
 function Set.new(t)
@@ -66,21 +99,20 @@ function Set.intersction(a,b)
     local result = Set.new({})
     for k in pairs(a) do
         if b[k] then
-            result[k] = b[k] 
+            result[k] = b[k]
         end
     end
     return result
 end
 
 function Set.contains_any(a, b)
-    for k in pairs(a) do
-        if b[k] then
+    for index, _ in pairs(a) do
+        if b[index] then
             return true
         end
     end
     return false
 end
-
 
 Set.mt.__bor = Set.contains_any -- Permite usar a | b ao invés de Set.contais_any(a, b)
 Set.mt.__mul = Set.intersction -- Permite usar a * b ao in vés de Set.intersection(a, b)
--- main.lua
+++ main.lua
@@ -3,51 +3,70 @@ local data_structures = require "data_structures"
 --[[
     Converte o array de transição em uma table de chaves do 
     caractere para ler concatenado com o estado atual
-    Ex: to: 1, read "a", viraria a chave "1a"
+    Ex: to: 1, read "a", vira a chave "1a"
 ]]
 
 Set = data_structures.Set
 DoubleLinkedList = data_structures.DoubleLinkedList
 
 local function convert_transition_and_finals(machine_table)
-    machine_table.finals = data_structures.Set.new(machine_table.finals)
-    for transition in machine_table["transitions"] do
-        for k, v in pairs(transition) do
-            local key = v.from .. v.read
-            local table = {}
-            table.to = v.to
-            table.write = v.write
-            table.go_right = true
-            if v.dir ~= "R" then
-                table.go_right = false
+    local new_transitions = {}
+    machine_table["final"] = Set.new(machine_table["final"]) 
+    for i, value in pairs(machine_table["transitions"]) do
+        local new_index = value["from"] .. value["read"]
+        local go_right = true
+        if value["dir"] == "L" then
+            go_right = false
+        end
+        new_transitions[new_index] = {to = value["to"], write = value["write"], dir = go_right}
+    end
+    machine_table["transitions"] = new_transitions
+end
+
+local function create_tape(str)
+    local tape = DoubleLinkedList.new()
+    for i = 1, string.len(str), 1 do
+        tape = DoubleLinkedList.append_right(tape, string.sub(str, i, i))
+    end
+    return tape
+end
+
+local function compute_turing_machine(machine, tape)
+    local current = machine.initial
+    local transitions = machine.transitions
+    while true do
+        local transition = transitions[current .. tape.info]
+        if not transition then
+            return false
+        end
+        current = transition.to
+        tape.info = transition.write
+        if transition.dir then
+            if not tape.next then
+                tape = DoubleLinkedList.append_right(tape, machine.white)
             end
-            transition[key] = table
-            transition[k] = nil
+            tape = tape.next
+        else
+            if not tape.before then
+                tape = DoubleLinkedList.append_left(tape, machine.white)
+            end
+            tape = tape.before
+        end
+        if Set.contains_any(Set.new({current}), machine.final) then
+            return true
         end
     end
 end
 
-local function create_tape(string)
-    local list = DoubleLinkedList.new()
-    for char in string do
-        list = DoubleLinkedList.append_right(list, char)
-    end
-    return list
-end
-
-local function compute_turing_machine(machine, tape)
-    current = Set.new(machine.current)
-end
-
 local function main()
     local machine_file;
-    local input_file;
     local machine_str = ""
-    local tape_str = ""
     local machine_table = {}
-    local tape_list = nil
+    local input_file;
+    local input_str = ""
+    local tape = nil
     if not arg[2] then
-       print("Sem argumentos para executar script")
+       error("Sem argumentos para executar script")
         return
     end
     machine_file = io.open(arg[1], "r")
@@ -60,17 +79,30 @@ local function main()
         machine_file.close(machine_file)
         error("Arquivo " .. arg[2] .. " nao encontrado")
     end
-    machine_str = input_file.read(input_file, "a")
-    machine_file.close(machine_file)
+    local output_file = io.open("saida.out", "w");
+    if not output_file then
+        machine_file:close()
+        input_file:close()
+        error("Incapaz de abrir/criar arquivo de saida")
+    end
+    machine_str = machine_file:read("a")
+    machine_file:close()
     machine_table = json.parse(machine_str)
     convert_transition_and_finals(machine_table)
     while true do
-        tape_list = create_tape(input_file.read(input_file, "l"))
-        if not tape_list then
+        input_str = input_file:read()
+        if input_str == "" or not input_str then
             break
         end
-
+        tape = create_tape(input_str)
+        if compute_turing_machine(machine_table, tape) then
+            print(1)
+        else
+            print(0)
+        end
+        output_file:write(DoubleLinkedList.convert_values_to_string(tape) .. "\n")
     end
+    output_file:close()
 end
 
 main()
